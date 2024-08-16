7a2a9887d9c04150981abfe8c5debc84c2761009
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-08-16 19:52:40 +0300

    first commit

---
+++ JavaScript/java.js
@@ -0,0 +1,19 @@
+// Lesson 2
+// Math and Numbers
+/* 
+Math operators
+Mathematic operations follow BODMAS => this is called order of precedence
+Add => +
+Subtraction => -
+Division => /
+Multiplication => *
+Association operator => ()
+Integers => 2,3,4
+Floating numbers/ decimal numbers => 2.333, 3.455
+When working with money , use cents and convert it back to the original currency
+
+Math Methods
+Math.round() => rounds a numbers to the nearest integer
+Math.ceil() => rounds a number up regardless of the position
+Math
+*/
